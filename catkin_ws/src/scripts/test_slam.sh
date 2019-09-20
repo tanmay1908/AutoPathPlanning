140920659f0df39de9b3115e0c1b10f29a924108
Tanmay Agarwal <agarw139@umn.edu>
2019-09-20 16:21:11 +0000

    feat: Made changes to scripts so that they refer to custom Gazebo world file and map files by not changing turtlbot package files themselves

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+
+xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find maps)/myworld.world" &
 sleep 5
 xterm  -e  "roslaunch gmapping slam_gmapping_pr2.launch" & 
 sleep 5
 xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 xterm  -e  "roslaunch turtlebot_teleop keyboard_teleop.launch"
+
+
